a1567481c550328117dbc9f209da02e53ae7afbd
MendelGusmao <mendelsongusmao@gmail.com>
2013-04-15 14:34:33 -0300

    Pass arguments from one job to another

--- jobs/base_job.rb
+++ jobs/base_job.rb
@@ -2,15 +2,17 @@ class BaseJob
     @all_config = nil
 
     def self.read
-        @all_config ||= YAML.load_file(File.dirname(__FILE__) + "/../torrent-done.yaml")
+        config_file = File.dirname(__FILE__) + "/../torrent-done.yaml"
+        @all_config ||= YAML.load_file(config_file)
     end
 
     def self.get_job_config
         read()[self.to_s]
     end
 
-    def self.perform(jobs, filename)
-        execute(filename)
-        Resque.enqueue(jobs.shift.constantize, jobs, filename) unless jobs.empty?
+    def self.perform(jobs, *args)
+        moreargs = execute(*args)
+        args << moreargs unless moreargs.nil?
+        Resque.enqueue(jobs.shift.constantize, jobs, *args) unless jobs.empty?
     end
 end
